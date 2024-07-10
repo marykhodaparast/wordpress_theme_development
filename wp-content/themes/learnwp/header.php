6d5465a44979f8d60a7ff16dd371a3af1782ff0b
“webbakery-maryam” <maryam@web-bakery.de>
2024-07-10 15:51:24 +0330

    add header and footer

---
+++ wp-content/themes/learnwp/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emmet</title>
+</head>
+<body <?php body_class();?>>
+    <p>This is just a paragraph</p>
+    <header>
+        <section class="top-bar">
+            <div class="social-media-icons"></div>
+            <div class="search"></div>
+        </section>
+        <section class="menu-area">
+            <section class="logo"></section>
+            <nav class="main-menu"></nav>
+        </section>
+    </header>
--- wp-content/themes/learnwp/index.php
+++ wp-content/themes/learnwp/index.php
@@ -1,41 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Emmet</title>
-</head>
-<body <?php body_class(); ?>>
-    <p>This is just a paragraph</p>
-    <header>
-        <section class="top-bar">
-            <div class="social-media-icons"></div>
-            <div class="search"></div>
+<?php get_header(); ?>
+<div class="content-area">
+    <main>
+        <section class="slide"></section>
+        <section class="services"></section>
+        <section class="middle-area">
+            <aside class="sidebar"></aside>
+            <div class="news">
+
+            </div>
+
         </section>
-        <section class="menu-area">
-            <section class="logo"></section>
-            <nav class="main-menu"></nav>
+        <section class="map">
+
         </section>
-
-    </header>
-    <div class="content-area">
-        <main>
-            <section class="slide"></section>
-            <section class="services"></section>
-            <section class="middle-area">
-                <aside class="sidebar"></aside>
-                <div class="news">
-
-                </div>
-
-            </section>
-            <section class="map">
-
-            </section>
-        </main>
-    </div>
-    <footer>
-        
-    </footer>
-</body>
-</html>
+    </main>
+</div>
+<?php get_footer(); ?>
